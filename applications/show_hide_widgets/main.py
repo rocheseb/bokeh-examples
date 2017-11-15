02d7f23000bbab62bd38c2bfc0fddf801d30e9ec
rocheseb <roche_sebastien@hotmail.fr>
2017-11-15 17:35:36 -0500

    better way to show/hide widgets in show_hide_widgets app

--- applications/show_hide_widgets/main.py
+++ applications/show_hide_widgets/main.py
@@ -7,32 +7,27 @@ stop_button = Button(label="Stop", disabled=True)
 m1_text = TextInput(title="mass of puck1 =", value="570")
 spark_timer = Slider(value=0, start=0, end=100, step=1, title="Title")
 
-button_map = {
-    "Start_Button": start_button,
-    "Stop_Button": stop_button, 
-    "Mass_of_m1": m1_text,
-    "Spark": spark_timer
-}
-button_call = Select(title="Button call", options=sorted(button_map.keys()), 
-value="Start_Button")
+button_list = ["Start_Button","Stop_Button","Mass_of_m1","Spark"]
+
+button_call = Select(title="Button call", options=sorted(button_list), value="Start_Button",width=200)
 
 select_code = """
 to_show = cb_obj.value;
 options = cb_obj.options
 
 for (i=0;i<options.length;i++) {
-	document.getElementsByClassName(options[i])[0].style.display = "None";
+	box = document.getElementsByClassName(options[i])[0];
+	if (!box.className.includes("hidden")) {box.className+=" hidden";}
+	if (box.className.includes(to_show)) {box.className=box.className.replace(" hidden","");}
 }
-
-document.getElementsByClassName(to_show)[0].style.display = "";
 """
 
 button_call.callback = CustomJS(code=select_code)
 
 box_1 = widgetbox(start_button,css_classes = ['Start_Button'])
-box_2 = widgetbox(stop_button,css_classes = ['Stop_Button'])
-box_3 = widgetbox(m1_text,css_classes = ['Mass_of_m1'])
-box_4 = widgetbox(spark_timer,css_classes = ['Spark'])
+box_2 = widgetbox(stop_button,css_classes = ['Stop_Button','hidden'])
+box_3 = widgetbox(m1_text,css_classes = ['Mass_of_m1','hidden'])
+box_4 = widgetbox(spark_timer,css_classes = ['Spark','hidden'])
 
 
 grid = gridplot([[button_call],[box_1,box_2,box_3,box_4]],toolbar_location=None)
