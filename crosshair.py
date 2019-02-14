187aa1ed78b48f14adcffb6481e226495a866349
rocheseb <roche_sebastien@hotmail.fr>
2019-02-14 11:19:21 -0500

    fixed callback

--- crosshair.py
+++ crosshair.py
@@ -5,22 +5,28 @@ from bokeh.plotting import figure, show, curdoc
 from bokeh.layouts import gridplot
 import numpy as np
 
-def addLinkedCrosshairs(plots):
+def addLinkedCrosshairs(plots,dimensions='both'):
 	"""
 	plots: list of figures
 	"""
-	js_move = '''	start = fig.x_range.start, end = fig.x_range.end
-					if(cb_obj.x>=start && cb_obj.x<=end && cb_obj.y>=start && cb_obj.y<=end)
-						{ cross.spans.height.computed_location=cb_obj.sx }
-					else { cross.spans.height.computed_location = null }
-					if(cb_obj.y>=start && cb_obj.y<=end && cb_obj.x>=start && cb_obj.x<=end)
-						{ cross.spans.width.computed_location=fig.plot_height-cb_obj.sy  }
-					else { cross.spans.width.computed_location=null }'''
-	js_leave = '''cross.spans.height.computed_location=null; cross.spans.width.computed_location=null'''
+	js_move = '''	start = fig.x_range.start;
+					end = fig.x_range.end;
+					if(cross.dimensions==="both" || cross.dimensions==="height"){
+						if(cb_obj.x>=start && cb_obj.x<=end && cb_obj.y>=start && cb_obj.y<=end)
+							{ cross.spans.height.computed_location=cb_obj.sx }
+						else { cross.spans.height.computed_location = null }
+					}
+					if(cross.dimensions==="both" || cross.dimensions==="width"){
+						if(cb_obj.y>=start && cb_obj.y<=end && cb_obj.x>=start && cb_obj.x<=end)
+							{ cross.spans.width.computed_location=fig.plot_height-cb_obj.sy  }
+						else { cross.spans.width.computed_location=null }
+					}'''
+	js_leave = '''	if(cross.dimensions==="both" || cross.dimensions==="height"){cross.spans.height.computed_location=null}; 
+					if(cross.dimensions==="both" || cross.dimensions==="width"){cross.spans.width.computed_location=null}'''
 
 	figures = plots[:]
 	for plot in plots:
-		crosshair = CrosshairTool(dimensions = 'both')
+		crosshair = CrosshairTool(dimensions=dimensions)
 		plot.add_tools(crosshair)
 		for figure in figures:
 			if figure != plot:
@@ -32,5 +38,5 @@ if __name__=="__main__":
 
 	plots = [figure(plot_width = 200, plot_height = 200, tools = '') for i in range(9)]
 	[plot.line(np.arange(10), np.random.random(10)) for plot in plots]
-	addLinkedCrosshairs(plots)
+	addLinkedCrosshairs(plots,dimensions='both')
 	show(gridplot(children = [plot for plot in plots], ncols = 3))
